35d9d75dae04d9e201d546419916ee6d0753a19e
Danilo Caballero <fabrizio.dfc2@gmail.com>
2016-11-11 00:29:06 -0800

    Production rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,41 +3,52 @@ import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import sass from 'rollup-plugin-sass';
+import uglify from 'rollup-plugin-uglify';
 
-export default {
+const config = {
   entry: 'source/index.js',
-  dest: 'build/index.js',
+  dest: 'build/app.js',
   format: 'iife',
   sourceMap: 'inline',
-  plugins: [
-    sass({
-      output: 'build/app.css',
-    }),
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true,
-    }),
-    commonjs({
-      exclude: 'node_modules/process-es6/**',
-      include: [
-        'node_modules/fbjs/**',
-        'node_modules/object-assign/**',
-        'node_modules/react/**',
-        'node_modules/react-dom/**',
-      ],
-      namedExports: {
-        'node_modules/react-dom/index.js': ['render'],
-      },
-    }),
-    babel({
-      babelrc: false,
-      exclude: 'node_modules/**',
-      presets: [['es2015', { modules: false } ], 'stage-0', 'react'],
-      plugins: ['external-helpers'],
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-  ]
 };
+
+const plugins = [
+  sass({
+    output: 'build/app.css',
+  }),
+  resolve({
+    jsnext: true,
+    main: true,
+    browser: true,
+  }),
+  commonjs({
+    exclude: 'node_modules/process-es6/**',
+    include: [
+      'node_modules/fbjs/**',
+      'node_modules/object-assign/**',
+      'node_modules/react/**',
+      'node_modules/react-dom/**',
+    ],
+    namedExports: {
+      'node_modules/react-dom/index.js': ['render'],
+    },
+  }),
+  babel({
+    babelrc: false,
+    exclude: 'node_modules/**',
+    presets: [['es2015', { modules: false } ], 'stage-0', 'react'],
+    plugins: ['external-helpers'],
+  }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+  }),
+];
+
+if (process.env.NODE_ENV == 'production') {
+  config.sourceMap = true;
+  plugins.push(uglify());
+}
+
+config.plugins = plugins;
+
+export default config;
